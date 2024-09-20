7c338fa2acfcb5a28e9ee89a974b32d1136016ab
NguyenPhamGiaKiet <kietnpg2752@ut.edu.vn>
2024-09-20 13:13:36 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Nhập số a
+        Console.Write("Nhập số a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        // Nhập số b
+        Console.Write("Nhập số b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        // Tính tổng a + b
+        int result = a + b;
+
+        // Hiển thị kết quả
+        Console.WriteLine($"{a} + {b} = {result}");
+    }
+}
